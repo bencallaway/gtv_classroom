b51853f3f0079269853718fa234dfc73dbd57b9f
Lenny Bogdonoff <lkbgift@gmail.com>
2013-01-26 13:23:45 -0800

    basic rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gtvclass::Application.config.secret_token = 'a84e66e97027561fe7eb87712c0610602f48d23496056967bed7607438ef67b5e93831c6e495615058d23fbdb9f35681249b9e7d9ff9de7c651d550d73cc2f41'
