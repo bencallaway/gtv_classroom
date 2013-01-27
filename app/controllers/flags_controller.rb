7b1749513faa583e8ad0fa8bdea077f8c5a4f3a1
Lenny Bogdonoff <lkbgift@gmail.com>
2013-01-27 14:59:41 -0800

    fixing flag pages

--- app/controllers/flags_controller.rb
+++ app/controllers/flags_controller.rb
@@ -2,7 +2,13 @@ class FlagsController < ApplicationController
 	def index
     respond_to do |format|
       format.json { render json: Flag.all }
-      format.html { @count = Flag.count }
+      format.html do
+        @count = Flag.count
+        @red_count = Answer.where(value: 'red').count
+        @green_count = Answer.where(value: 'green').count
+        @blue_count = Answer.where(value: 'blue').count
+        @yellow_count = Answer.where(value: 'yellow').count
+      end
     end
 	end
 
